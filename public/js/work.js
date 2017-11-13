d1394b4385ed7c54422d9da956c5c81a6bd61812
michael-montgomery <montgomery.michael@hotmail.com>
2017-11-13 18:24:33 -0500

    updated projects.

--- public/js/work.js
+++ public/js/work.js
@@ -9,11 +9,6 @@ app.controller('workController', function($scope) {
             link: 'https://michael-montgomery.github.io/legacy/#/welcome'
         },
         {
-            name: 'Luna',
-            backgroundUrl: 'https://www.sourpussclothing.com/wordpress/wp-content/uploads/2015/01/LUNA_MARIE_2.jpg',
-            link: 'http://www.michaelmontgomery.io/luna/#/home'
-        },
-        {
             name: 'Health First',
             backgroundUrl: 'http://hlknweb.tamu.edu/sites/hlknweb.tamu.edu/files/styles/main_page_photo/public/health%20check.jpg?itok=aAKbZUcC',
             link: 'http://www.michaelmontgomery.io/hfhp/#/welcome'
@@ -22,11 +17,6 @@ app.controller('workController', function($scope) {
             name: 'WMTEK',
             backgroundUrl: 'https://static.pexels.com/photos/161281/cross-christian-religion-church-161281.jpeg',
             link: 'https://michael-montgomery.github.io/wmtekdemo/#/welcome'
-        },
-        {
-            name: 'Pixi',
-            backgroundUrl: 'https://static.pexels.com/photos/5317/food-salad-restaurant-person.jpg',
-            link: 'https://michael-montgomery.github.io/food/#!/welcome'
         }
     ];
 
